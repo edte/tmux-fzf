88670855a9e5a0682cd3c9ed3d36f2c16f8dde60
edte <zzzzip6@gmail.com>
2024-12-26 20:01:28 +0800

    delete .envs files

--- scripts/last_window.sh
+++ scripts/last_window.sh
@@ -1,6 +1,7 @@
-#!/bin/zsh
+#!/bin/bash
 
 # tmux 跳转到前一个window，不区分session
+
 current_session=$(tmux display-message -p '#{session_name}')
 current_window=$(tmux display-message -p '#{window_index}')
 current_window_name=$(tmux display-message -p '#{window_name}')
@@ -8,35 +9,32 @@ current_window_name=$(tmux display-message -p '#{window_name}')
 tmp_file="/tmp/tmux_previous"
 
 if [ -f "$tmp_file" ]; then
-    IFS=$'\n' read -d '' -A previous_values < "$tmp_file"
-    previous_session="${previous_values[1]}"
-    previous_window="${previous_values[2]}"
+    IFS=$'\n' read -d '' -r -a previous_values <"$tmp_file"
+    previous_session="${previous_values[0]}"
+    previous_window="${previous_values[1]}"
 else
     previous_session=""
     previous_window=""
 fi
 
 if [ -z "$previous_session" ] || [ -z "$previous_window" ]; then
-    echo -e "$current_session\n$current_window" > "$tmp_file"
+    echo -e "$current_session\n$current_window" >"$tmp_file"
     echo "first init"
-    echo "previous": $previous_session:$previous_window
-    echo "current": $current_session:$current_window
-    return
+    echo "previous: $previous_session:$previous_window"
+    echo "current: $current_session:$current_window"
+    exit
 fi
-
 
 # echo $previous_session
 # echo $previous_window
 
 if [ "$current_session" = "$previous_session" ] && [ "$current_window" = "$previous_window" ]; then
     echo "Current and previous windows are the same. Doing nothing."
-    return
+    exit
 fi
-
 
 tmux switch-client -t "${previous_session}:${previous_window}"
 
+# echo "current: $current_session:$current_window:$current_window_name"
 
-# echo "current": $current_session:$current_window:$current_window_name
-
-echo -e "$current_session\n$current_window" > "$tmp_file"
+echo -e "$current_session\n$current_window" >"$tmp_file"
--- scripts/switch_window.sh
+++ scripts/switch_window.sh
@@ -1,23 +1,25 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+# 切换window，不区分session
 
 current_session=$(tmux display-message -p '#{session_name}')
 current_window_index=$(tmux display-message -p '#{window_index}')
 current_window_name=$(tmux display-message -p '#{window_name}')
 
 
-CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "$CURRENT_DIR/.envs"
+TMUX_FZF_BIN="$(which fzf-tmux)"
 
-current_window_origin=$(tmux display-message -p '#S:#I: #{window_name}')
 current_window=$(tmux display-message -p '#S:#I:')
 
-
-windows=$(tmux list-windows -a -F "#S:#{window_index}: #{window_name}" | grep -v "^$current_window" )
+windows=$(tmux list-windows -a -F "#S:#{window_index}: #{window_name}" | grep -v "^$current_window")
 
 # $TMUX_FZF_BIN 是 fzf 可执行文件的路径，$TMUX_FZF_OPTIONS 和 $TMUX_FZF_PREVIEW_OPTIONS 是传递给 fzf 的选项。
 # fzf 是一个命令行模糊查找器，用于在列表中选择一个项目。最后，用户选择的项目（窗口）存储在 target_origin 变量中。
-select_window=$(printf  "$windows" | eval "$TMUX_FZF_BIN -p -w 90% -h 90% -m --preview='/Users/edte/go/src/test/tmux-fzf/scripts/.preview {}' --preview-window=:follow")
-[[  -z "$select_window" ]] && exit
+select_window=$(printf "$windows" | eval "$TMUX_FZF_BIN -p -w 90% -h 90% -m --preview='{}' --preview-window=:follow")
+
+echo "$TMUX_FZF_BIN -p -w 90% -h 90% -m --preview=' {}' --preview-window=:follow"
+
+[[ -z "$select_window" ]] && exit
 
 # 这行代码使用 sed 命令删除 target_origin 中的 : 之后的所有内容，只保留窗口的索引或名称。处理后的结果存储在 target 变量中。
 target=$(echo "$select_window" | sed 's/: .*//')
@@ -28,7 +30,6 @@ echo "$target" | sed 's/:.*//g' | xargs -I{} tmux switch-client -t {}
 
 # 这行代码使用 xargs 命令将 target 变量传递给 tmux select-window -t 命令，从而激活并切换到目标窗口。
 echo "$target" | xargs -I{} tmux select-window -t {}
-
 
 new_session=$(tmux display-message -p '#{session_name}')
 new_window=$(tmux display-message -p '#{window_index}')
@@ -41,4 +42,4 @@ fi
 
 tmp_file="/tmp/tmux_previous"
 
-echo -e "$current_session\n$current_window_index" > "$tmp_file"
+echo -e "$current_session\n$current_window_index" >"$tmp_file"
